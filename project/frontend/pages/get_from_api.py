42a93a6ea62ce4dce66adc6441e8079beeb07f6c
user <bigpigeonp@gmail.com>
2024-06-27 16:14:53 +0200

    add the frontend folder with a main page, the index.py which will be the file to start streamlit

---
+++ project/frontend/pages/get_from_api.py
@@ -0,0 +1,12 @@
+import streamlit as st
+import requests
+
+st.title("get information from api")
+
+if st.button("Call"):
+    response = requests.get(url = "http://127.0.0.1:8000/get-from-api")
+    message = response.json()['response']
+    st.write(f"response from api: [{message}]")
+
+
+
