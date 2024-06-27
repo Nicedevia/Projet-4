2ab374ae9eef25b275cf9cf89b6926da52bf8faa
briac <briacsavoie@gmail.com>
2024-06-27 16:32:43 +0200

    test do not merge

--- database/connextion.py
+++ database/connextion.py
@@ -2,18 +2,19 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import psycopg2
+DATABASE_URL = "postgresql://postgres.mmsucpjcvccdxjhymgrc:Elidevia2023!@aws-0-eu-central-1.pooler.supabase.com:6543/postgres"
 
-database_url = "postgresql://postgres.mmsucpjcvccdxjhymgrc:Elidevia2023!@aws-0-eu-central-1.pooler.supabase.com:6543/postgres"
-
-engine = create_engine(database_url)
+engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
+
 
 try:
     with engine.connect() as conn:
         print("Connected to the database.")
 except Exception as e:
     print(f"Unable to connect to the database: {e}")
-
+    
 
 def get_db():
     db = SessionLocal()
