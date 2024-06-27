4912517be159f2a5b79e5bb378501755138e9288
Finov30 <samu.abid@gmail.com>
2024-06-27 11:35:29 +0200

    First Test Push

--- main.py
+++ main.py
@@ -10,9 +10,13 @@ class User(BaseModel):
     password: str  
 
 @app.get("/items/{user}")
-def read_item(user: int, q: Union[str, None] = None):
-    return {"user": user, "id": 5, "email": "sam@gmail.com"}
+def read_item(user: int, id : int, email : str,):
+    return {"user": user, "id": id, "email": email}
 
 @app.put("/items/{user}")
-def update_item(user: int, q: Union[str, None] = None):
+def update_item(user: int, id : int, email : str,):
     return {"message": "User updated successfully"}
+
+@app.delete("/items/{user}")
+def delete_item(id : int):
+    return {"message": "User deleted"}
