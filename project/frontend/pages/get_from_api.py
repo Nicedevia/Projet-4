dd62c541a8824a48fb7b5f0eaa21702dd6e7b06d
user <bigpigeonp@gmail.com>
2024-06-28 15:03:59 +0200

    edit the called route because of the app folder architecture changes, why changing the template? why putting every routes into the auth routes folder? idfk

--- project/frontend/pages/get_from_api.py
+++ project/frontend/pages/get_from_api.py
@@ -4,7 +4,7 @@ import requests
 st.title("get information from api")
 
 if st.button("Call"):
-    response = requests.get(url = "http://127.0.0.1:8000/get-from-api")
+    response = requests.get(url = "http://127.0.0.1:8000/auth/get-from-api")
     message = response.json()['response']
     st.write(f"response from api: [{message}]")
 
